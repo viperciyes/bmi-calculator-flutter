9d21431301d2ac64a1490273b56875e60b2b0d97
viperciyes <viperciyes@gmail.com>
2021-01-03 03:04:25 +0300

    weas

---
+++ lib/calculator_brain.dart
@@ -0,0 +1,33 @@
+import 'dart:math';
+
+class CalculatorBrain {
+  CalculatorBrain(this.height, this.weight);
+  final int height;
+  final int weight;
+
+  double _bmi;
+  String calculateBMI() {
+    _bmi = weight / pow(height / 100, 2);
+    return _bmi.toStringAsFixed(1);
+  }
+
+  String getResult() {
+    if (_bmi >= 25) {
+      return 'Biraz fazlanız var..';
+    } else if (_bmi > 18.5) {
+      return 'Normal';
+    } else {
+      return 'Biraz kilo almalısınız..';
+    }
+  }
+
+  String getInterpretation() {
+    if (_bmi >= 25) {
+      return 'Daha fazla egzersiz yapmalısınız..';
+    } else if (_bmi > 18.5) {
+      return 'Bravo..';
+    } else {
+      return 'Biraz daha fazla yemeniz gerek..';
+    }
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,34 +1,23 @@
 import 'package:flutter/material.dart';
 
+import 'input_page.dart';
+import 'results_page.dart';
+
 void main() => runApp(BMICalculator());
 
 class BMICalculator extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: InputPage(),
-    );
-  }
-}
-
-class InputPage extends StatefulWidget {
-  @override
-  _InputPageState createState() => _InputPageState();
-}
-
-class _InputPageState extends State<InputPage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('BMI CALCULATOR'),
+      theme: ThemeData.dark().copyWith(
+        primaryColor: Color(0xFF04040C),
+        scaffoldBackgroundColor: Color(0xFF04040C),
       ),
-      body: Center(
-        child: Text('Body Text'),
-      ),
-      floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.add),
-      ),
+      initialRoute: '/',
+      routes: {
+        '/': (context) => InputPage(),
+        ResultsPage.routeName: (context) => ResultsPage(),
+      },
     );
   }
 }
